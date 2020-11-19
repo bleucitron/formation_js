5d9bacd2a4a23fded79ae3af4ba37051d1506047
Romain Crestey <rcrestey@gmail.com>
2020-11-19 11:47:42 +0100

    2020-11-19

--- exos/4_async/index.js
+++ exos/4_async/index.js
@@ -9,6 +9,20 @@
     - Sinon, afficher "Pas cool..."
 */
 
+// const p = createRandomNumber();
+
+// p.then(function (valeurPromise) {
+//   console.log('Valeur promise', valeurPromise);
+
+//   if (valeurPromise > 50) {
+//     console.log('Cool');
+//   } else {
+//     console.log('Pas cool...');
+//   }
+// });
+
+// console.log('P', p);
+
 /* Exercice 2: Temps d'attente
     - Vous avez accès à une fonction waitForNumber() qui crée une Promesse d'attendre un certain temps.
     - Si ce temps est trop long, la Promesse est rejetée, sinon elle est résolue avec le temps d'attente.
@@ -17,6 +31,22 @@
     - Essayer de trouver quel est le temps d'attente maximal
 */
 
+let max = 0;
+
+function wait() {
+  waitForNumber()
+    .then(function (vp) {
+      max = Math.max(max, vp);
+    })
+    .catch(function (err) {
+      wait();
+    });
+}
+
+for (let i = 0; i < 500; i++) {
+  wait();
+}
+
 /* Exercice 3: Charger des tweets
     - Vous avez accès à une URL qui contient des tweets.
     - Charger et logguer les tweets en utilisant fetch()
@@ -24,3 +54,26 @@
 
 const url =
   'https://raw.githubusercontent.com/iOiurson/data/master/data/tweets.json';
+const url2 =
+  'https://raw.githubusercontent.com/iOiurson/data/master/data/tweets2.json';
+
+const data1 = fetch(url);
+const data2 = fetch(url2);
+
+const l1 = data1.then(function (httpResp) {
+  return httpResp.json();
+});
+
+const l2 = data2.then(function (httpResp) {
+  return httpResp.json();
+});
+
+const lAll = [l1, l2];
+
+Promise.all(lAll)
+  .then(function (result) {
+    console.log('RESULT', result.flat());
+  })
+  .catch(function (err) {
+    console.log('ERROR', err);
+  });
